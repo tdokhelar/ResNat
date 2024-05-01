abb33059f5d783413ea86da7927f5cea9d682f68
tdokhelar <84328717+tdokhelar@users.noreply.github.com>
2024-05-01 11:14:43 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Initialiser GoGoCartoJs avec les options spécifiques
-var options = {
-    target: 'map', // ID de l'élément contenant la carte
-    endpoint: 'https://resnat.gogocarto.fr' // Endpoint de l'API de Gogocarto
-};
-
-var gogocarto = new GoGoCarto(options);
