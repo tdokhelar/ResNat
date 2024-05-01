164eeb646199f4f206ac4851963d815ad8e1c00c
tdokhelar <84328717+tdokhelar@users.noreply.github.com>
2024-05-01 11:11:03 +0200

    Add files via upload

---
+++ classes/config/gogo-config.class.ts
@@ -0,0 +1,234 @@
+import { App } from "../../gogocarto";
+import { TileLayer } from '../map/tile-layer.class';
+import { GoGoFeature } from './gogo-feature.class';
+import { ElementStatus } from '../classes'; 
+import { DEFAULT_FEATURES } from './gogo-default-feature' ;
+declare var L : any;
+
+export class GoGoConfig
+{
+  readonly text =
+  {
+    element: 'élément',
+    elementDefinite: "l'élément",
+    elementIndefinite: "un élément",
+    elementPlural: 'éléments',
+    collaborativeModeration: `<p>
+        Lorsqu'un élément est ajouté ou modifié, la mise à jour des données n'est pas instantanée. L'élément va d'abords apparaître "grisé" sur la carte,
+        et il sera alors possible à tous les utilisateurs logué de voter une et une seule fois pour cet élément. 
+        Ce vote n'est pas une opinion, mais un partage de connaissance. 
+        Si vous connaissez cet élément, ou savez que cet élément n'existe pas, alors votre savoir nous intéresse !
+      </p> 
+      <p>
+        Au bout d'un certain nombre de votes, l'élément pourra alors être automatiquement validé ou refusé. 
+        En cas de litige (des votes à la fois positifs et négatifs), un modérateur interviendra au plus vite. On compte sur vous!
+      </p>`
+  };
+  readonly data =
+  {
+    taxonomy: undefined,
+    elements: undefined,
+    elementsCompactApiUrl: undefined,
+    elementByIdUrl: undefined,
+    requestByBounds: false,
+    retrieveElementsByApi: false,
+    showPending: true,
+  };
+  readonly menu =
+  {
+    width: undefined,
+    smallWidthStyle: false,
+    showOnePanePerMainOption: false,
+    showCheckboxForMainFilterPane: true,
+    showCheckboxForSubFilterPane: true,    
+  };
+  readonly infobar =
+  {
+    width: undefined,
+    activate: true,
+    bodyTemplate: {
+      content: undefined,
+      type: "string", // string | url
+      isMarkdown: true
+    },
+    headerTemplate: {
+      content: undefined,
+      type: "string", // string | url
+      isMarkdown: true
+    }
+  };
+  readonly general =
+  {
+    activateHistoryStateAndRouting: true
+  };
+  readonly map =
+  {
+    // france
+    defaultBounds : L.latLngBounds(L.latLng(52, 10), L.latLng(40, -5)),
+    defaultBoundsProvided : false,
+    defaultCenter : L.latLng(46, 0),
+    maxBounds : L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180)),
+    saveViewportInCookies : false,
+    saveTileLayerInCookies : false,
+    defaultTileLayer : 'cartodb',
+    tileLayers : [
+      // new TileLayer('mapbox', 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2ViYWxsb3QiLCJhIjoiY2l4MGtneGVjMDF0aDJ6cWNtdWFvc2Y3YSJ9.nIZr6G2t08etMzft_BHHUQ'),
+      // new TileLayer('mapboxlight', 'https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2ViYWxsb3QiLCJhIjoiY2l4MGtneGVjMDF0aDJ6cWNtdWFvc2Y3YSJ9.nIZr6G2t08etMzft_BHHUQ'),
+      new TileLayer('cartodb', 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'), 
+      new TileLayer('hydda', 'https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png'), 
+      new TileLayer('wikimedia', 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png'), 
+      new TileLayer('monochrome', 'https://www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png'), 
+      new TileLayer('lyrk ', 'https://tiles.lyrk.org/ls/{z}/{x}/{y}?apikey =982c82cc765f42cf950a57de0d891076'), 
+      new TileLayer('osmfr', 'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'),
+      new TileLayer('stamen', 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png'),
+      new TileLayer('stamenTerrain', 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png'),    
+      new TileLayer('stamenWaterColor', 'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png'),    
+      new TileLayer('openriver', 'https://{s}.tile.openstreetmap.fr/openriverboatmap/{z}/{x}/{y}.png'),
+      new TileLayer('thunderforest', 'https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png'),
+      new TileLayer('Pas de fond', '') ]
+  };
+  readonly features =
+  {
+    // element menu
+    favorite:       new GoGoFeature(),
+    share:          new GoGoFeature(),
+    directions:     new GoGoFeature(),
+
+    // element content
+    sendMail:       new GoGoFeature(),
+    elementHistory: new GoGoFeature(),
+
+    // gogo controls
+    listMode: new GoGoFeature(),
+
+    // map controls
+    export:          new GoGoFeature(),
+    layers:          new GoGoFeature(),
+    mapdefaultview:  new GoGoFeature(),
+    
+    pending:    new GoGoFeature(),    
+    searchPlace:      new GoGoFeature(),
+    searchElements:   new GoGoFeature(),
+    searchGeolocate:  new GoGoFeature(),
+
+    // element interaction / moderation
+    edit:       new GoGoFeature(),          
+    delete:     new GoGoFeature(),
+    report:     new GoGoFeature(),      
+    vote:       new GoGoFeature(),
+    moderation: new GoGoFeature(),
+    stamp:      new GoGoFeature(),
+    directModeration:        new GoGoFeature(),
+
+    // others
+    customPopup: new GoGoFeature(),
+  };
+  readonly security =
+  {
+    userRoles: ['anonymous'],
+    userEmail: '',
+    loginAction: function() { console.warn("[GoGoCarto] You need login to access this feature"); },
+
+    hideMailsByShowingSendMailButton: true,
+  };
+
+  readonly theme = 'default';
+  // see gogo-styles for defaut values
+  readonly colors =
+  {
+    neutralDark: undefined ,
+    neutralDarkTransparent: undefined ,
+    neutralSoftDark: undefined ,
+    neutral: undefined ,
+    neutralLight: undefined ,
+    secondary: undefined ,
+    primary: undefined ,
+    background: undefined ,
+
+    textColor: undefined ,
+    disableColor: undefined ,
+    listTitle: undefined ,
+    listTitleBackBtn: undefined,
+    listTitleBackground: undefined ,
+    searchBar: undefined, 
+    interactiveSection: undefined,
+
+    mainFont: undefined ,
+    titleFont: undefined ,
+    taxonomyMainTitleFont: undefined ,     
+  }
+
+  readonly images =
+  {
+    buttonOpenMenu: undefined,
+    menuTopImage: undefined
+  }
+
+	constructor(config : any)
+	{
+    if (!config.features) config.features = DEFAULT_FEATURES;
+    // Copy all the defined options
+    // All the options non specified will be initialized with default values
+    this.recursiveFillProperty(this, config);
+    this.data.retrieveElementsByApi = typeof this.data.elements == "string";
+    if (config.map && config.map.defaultBounds) this.map.defaultBoundsProvided = true;
+    if (!this.features['sendMail'].active) this.security.hideMailsByShowingSendMailButton = false;
+    console.log(this);
+	}
+  
+  isFeatureActivated(featureName) : boolean
+  {
+    if (!this.features.hasOwnProperty(featureName)) { console.warn(`[GoGoCartoJs] feature ${featureName} doesn't exist`); return false; }
+
+    return this.features[featureName].active && (!App.isIframe || this.features[featureName].inIframe);
+  }
+
+  // is feature is activated and the actual user is granted to use it
+  isFeatureAvailable(featureName) : boolean
+  {
+    if (!this.features.hasOwnProperty(featureName)) { console.warn(`[GoGoCartoJs] feature ${featureName} doesn't exist`); return false; }
+
+    let feature = this.features[featureName];
+
+    let roleProvided = true;
+    if (feature.hasOwnProperty('roles'))
+    {
+      roleProvided = feature.hasRole(App.loginModule.getRoles());
+    }
+
+    return this.isFeatureActivated(featureName) && roleProvided;
+  } 
+
+  private recursiveFillProperty(that, object)
+  {
+    // we don't want to apply recursively inside objects properties
+    let objectsProperties = ['roles', 'defaultCenter', 'defaultBounds', 'tileLayers', 'options'];
+
+    // if we provide feature config, we enable it automatically
+    if (that instanceof GoGoFeature) that.active = true;
+
+    for(var prop in object) 
+    {
+        if (that.hasOwnProperty(prop))
+        {
+          if (typeof that[prop] != 'object' || objectsProperties.indexOf(prop) > -1)
+          {
+            let new_prop;
+            switch(prop) {
+              case 'defaultBounds' : new_prop = L.latLngBounds(object[prop]);break;
+              case 'defaultCenter' : new_prop = L.latLng(object[prop]);break;
+              default: new_prop = object[prop];break;
+            }
+            that[prop] = new_prop;
+          }
+          else            
+            this.recursiveFillProperty(that[prop], object[prop]);
+        }
+        else
+        {
+          console.warn("[GoGoCarto] Config option '" + prop + "' does not exist");
+        }
+    }
+  }
+
+}
---
+++ classes/element/element.class.ts
@@ -0,0 +1,175 @@
+/**
+ * This file is part of the GoGoCarto project.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) 2016 Sebastian Castro - 90scastro@gmail.com
+ * @license GNU GPL v3
+ * @Last Modified time: 2016-12-13
+ */
+import { AppModule, AppStates, AppModes } from "../../app.module";
+import { ElementBase, ElementStatus, ElementModerationState } from './element-base.class';
+export { ElementStatus, ElementModerationState } from './element-base.class';
+import { Marker } from "../../components/map/marker.component";
+import { ElementComponent } from "../../components/element/element.component";
+import { OptionValue, CategoryValue, Option, Category, Contribution, VoteReport, Stamp } from "../classes";
+import { capitalize } from "../../utils/string-helpers";
+
+import { App } from "../../gogocarto";
+declare var $, Map;
+
+export class Element extends ElementBase
+{	
+	private marker_ : Marker = null;
+	private component_ : ElementComponent = null;	
+
+	colorOptionId : any;
+
+	private isInitialized_ : boolean = false;
+
+	iconsToDisplay : OptionValue[] = [];	
+
+	distance : number;
+	distanceFromBoundsCenter : number;	
+
+	// for elements module algorithms
+	isDisplayed : boolean = false;	
+
+	isShownAlone : boolean = false;
+	isFavorite : boolean = false;
+	needToBeUpdatedWhenShown : boolean = true;
+
+	constructor(elementJson : any)
+  {
+    super(elementJson);  
+  }
+
+  updateWithJson(elementJson)
+  {
+  	super.updateWithJson(elementJson);
+  	this.createOptionsTree();
+    this.update(true);
+  }	
+
+	initialize() 
+	{		
+		App.elementIconsModule.updateIconsToDisplay(this);
+
+		this.marker_ = new Marker(this.id, this.position);
+		this.isInitialized_ = true;	
+	}
+
+	update($force : boolean = false)
+	{
+		//console.log("marker update needToBeUpdated", this.needToBeUpdatedWhenShown);
+		if (this.needToBeUpdatedWhenShown || App.mode == AppModes.List || $force)
+		{
+			App.elementIconsModule.updateIconsToDisplay(this);
+			let optionValuesToUpdate = this.getCurrOptionsValues().filter( (optionValue) => optionValue.isFilledByFilters);
+			optionValuesToUpdate.push(this.getCurrMainOptionValue());
+			for(let optionValue of optionValuesToUpdate) App.elementOptionValuesModule.updateOptionValueColor(this, optionValue);
+
+			this.colorOptionId = this.iconsToDisplay.length > 0 && this.getIconsToDisplay()[0] ? this.getIconsToDisplay()[0].colorOptionId : null;	
+
+			if (this.marker) this.marker.update();
+			this.needToBeUpdatedWhenShown = false;
+		}		
+	}
+
+	updateDistance()
+	{
+		this.distance = null;
+		this.distanceFromBoundsCenter = App.boundsModule.extendedBounds ? App.boundsModule.extendedBounds.getCenter().distanceTo(this.position) / 1000 : null;
+
+		if (App.geocoder.getLocation()) 
+			this.distance = App.mapComponent.distanceFromLocationTo(this.position);
+		else
+			this.distance = this.distanceFromBoundsCenter;
+		
+		// Making the distance more realistic multiplying
+		this.distance = this.distance ? Math.round(1.2*this.distance) : null;
+		this.distanceFromBoundsCenter = this.distanceFromBoundsCenter ? Math.round(1.2*this.distanceFromBoundsCenter) : null;
+	}
+
+	getIconsToDisplay() : OptionValue[]
+  {
+    let result = this.iconsToDisplay;
+    return result.sort( (a,b) => a.isFilledByFilters ? -1 : 1);
+  }
+
+	getCurrOptionsValues() : OptionValue[]
+	{
+		if (App.config.menu.showOnePanePerMainOption) return this.optionsValues.filter( (optionValue) => optionValue.option.mainOwnerId == App.currMainId);
+		return this.optionsValues;
+	}
+
+	getCurrDeepestOptionsValues() : OptionValue[]
+	{
+		let currOptionValues = this.getCurrOptionsValues();
+    let idsToRemove = []
+    for (let ov of currOptionValues) idsToRemove = idsToRemove.concat(ov.option_.parentOptionIds);
+    let deepestOv = currOptionValues.filter( (oV) => idsToRemove.indexOf(oV.option_.id) == -1);
+
+  	// group by owner
+  	let groupedByParentOvs = {}
+  	for (let ov of deepestOv) { 
+  		let parentName = ov.option.parentOptionName;
+  		if (parentName in groupedByParentOvs) groupedByParentOvs[parentName].push(ov); 
+  		else groupedByParentOvs[parentName] = [ov]; 
+  	}
+  	let deepestOrderedOv = [];
+  	for (let parent in groupedByParentOvs) { deepestOrderedOv = deepestOrderedOv.concat(groupedByParentOvs[parent]); }
+  	return deepestOrderedOv;
+	}
+
+	getCurrMainOptionValue() : OptionValue
+	{
+		return this.optionsValues.filter( (optionValue) => optionValue.option.id == App.currMainId).shift();
+	}
+
+	getCategoriesIds() : number[]
+	{
+		return this.getCurrOptionsValues().map( (optionValue) => optionValue.categoryOwner.id).filter((value, index, self) => self.indexOf(value) === index);
+	}
+
+	getOptionsIdsInCategorieId(categoryId) : number[]
+	{
+		return this.getCurrOptionsValues().filter( (optionValue) => optionValue.option.ownerId == categoryId).map( (optionValue) => optionValue.optionId);
+	}	
+
+	displayStamps() : Stamp[]
+	{
+		return App.stampModule.getAllowedStampForElement(this);
+	}
+
+	isPending() { return this.status == ElementStatus.PendingAdd || this.status == ElementStatus.PendingModification; }
+	isDeleted() { return this.status <= ElementStatus.AdminRefused }
+	needsModeration() { return this.moderationState != ElementModerationState.NotNeeded }	
+
+	get marker() : Marker { return this.marker_; }
+	get component() { return this.component_ || (this.component_ = new ElementComponent(this)); }	
+	get isInitialized() { return this.isInitialized_; }
+
+  toDisplay()
+  {
+    let result = {
+      name: this.formatProp('name'),
+      address: this.formatProp('address'),
+      originalAddress: this.address,
+      distance: this.distance,
+      taxonomy: this.gogoTaxonomy(),
+      status: this.status,
+      isPending: this.isPending(),
+      isDeleted: this.isDeleted(),
+      needsModeration: this.needsModeration(),
+      formatedOpenHours: this.formatedOpenHours
+    };
+    $.each(this.data, (key, value) => {
+       if(!(key in result)) result[key] = this.formatProp(key)
+    });
+    return result; 
+  }
+
+  formatProp(propertyName) { return  App.elementFormaterModule.getProperty(this, propertyName); }
+}
+
